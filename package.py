72081e1f7385c6178c8207c503cbf27cf5a1efc0
Adam Shimali <adam.shimali@gmail.com>
2019-01-23 12:16:58 +0000

    Package using external schema rather than inferred.

--- package.py
+++ package.py
@@ -3,7 +3,9 @@
 import os
 import click
 
-from datapackage import Package
+from datapackage import Package, Resource
+from tableschema import Schema
+from datapackage.exceptions import CastError
 
 
 @click.command()
@@ -11,13 +13,32 @@ from datapackage import Package
 def make_package(source):
 
     os.chdir(source)
+    files = os.listdir('data')
+
     package = Package()
-    package.infer('**/*.csv')
+
+    for f in files:
+        path = f"data/{f}"
+        name = f.replace('.csv', '')
+        schema = f"https://raw.githubusercontent.com/digital-land/alpha-data/master/schema/{name}-schema.json"
+        resource = Resource({'path': path, 'schema': schema})
+        package.add_resource(resource.descriptor)
+
     package.commit()
+    package.infer()
 
-    print(package.valid)
+    errors = False
+    for r in package.resources:
+        try:
+            r.read(keyed=True)
+        except CastError as e:
+            print('Error in', os.path.join(source, r.descriptor['path']))
+            print(e, e.errors)
+            errors = True
+    if not errors:
+        package.save('datapackage.json')
+        print('saved datapackage.json to', source)
 
-    package.save('datapackage.json')
 
 
 if __name__ == '__main__':
